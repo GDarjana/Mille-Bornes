967820a662eb193ae5822c6ef5718ec8640c2b5f
GDarjana <zarkashnc@yahoo.com>
2022-05-15 17:55:32 +1100

    Màj Carte & CarteInterface

--- src/main/java/nc/unc/gl/borne/Metier/Carte/CarteInterface.java
+++ src/main/java/nc/unc/gl/borne/Metier/Carte/CarteInterface.java
@@ -1,14 +1,12 @@
 package nc.unc.gl.borne.Metier.Carte;
 
-import nc.unc.gl.borne.Metier.Joueur.Joueur;
-
 public interface CarteInterface {
     public String getEffet();
 
     public String getType();
 
-    public void appliquerEffet(Joueur cible);
-
     public String getPathImage();
+
+    public String getDescription();
 
 }
