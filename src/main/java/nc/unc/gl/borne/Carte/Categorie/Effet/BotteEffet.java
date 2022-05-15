428fa7f032d88b72babd31eb1fe70b597edb547a
GDarjana <zarkashnc@yahoo.com>
2022-05-15 14:27:09 +1100

    Correction description

--- src/main/java/nc/unc/gl/borne/Carte/Categorie/Effet/BotteEffet.java
+++ src/main/java/nc/unc/gl/borne/Carte/Categorie/Effet/BotteEffet.java
@@ -1,10 +1,10 @@
 package nc.unc.gl.borne.Carte.Categorie.Effet;
 
 public enum BotteEffet {
-    VEHICULE_PRIORITAIRE("Immunise contre les attaque de limites de vitesse", "botte_vitesse.jpeg"),
-    CITERNE_ESSENCE("Immunise contrel es attaques de panne d'essence", "botte_essence.jpeg"),
+    VEHICULE_PRIORITAIRE("Immunise contre les attaques de limites de vitesse", "botte_vitesse.jpeg"),
+    CITERNE_ESSENCE("Immunise contre les attaques de panne d'essence", "botte_essence.jpeg"),
     INCREVABLE("Immunise contre les attaques de crevaison", "botte_crevaison.jpeg"),
-    AS_DU_VOLANT("Immunise contre les attaque accident", "botte_accident.jpeg");
+    AS_DU_VOLANT("Immunise contre les attaques accident", "botte_accident.jpeg");
 
     private final String pathToImage;
     private final String description;
